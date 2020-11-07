baaee1539ef1b835c78ed5305354fdf92308c12a
adityatripathiiit <adityatripathiball@gmail.com>
2020-11-08 01:21:08 +0530

    fixed

--- common_threads.h
+++ common_threads.h
@@ -28,10 +28,5 @@
 #define Cond_signal(cond)                                assert(pthread_cond_signal(cond) == 0);
 #define Cond_wait(cond, mutex)                           assert(pthread_cond_wait(cond, mutex) == 0);
 
-#ifdef __linux__
-#define Sem_init(sem, value)                             assert(sem_init(sem, 0, value) == 0);
-#define Sem_wait(sem)                                    assert(sem_wait(sem) == 0);
-#define Sem_post(sem)                                    assert(sem_post(sem) == 0);
-#endif // __linux__
 
 #endif // __common_threads_h__
